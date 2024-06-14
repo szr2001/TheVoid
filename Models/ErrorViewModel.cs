776ad00bbb7ca38530bea97831a262ca6809b3a8
Szabo Robert <szr2001@yahoo.com>
2024-06-14 17:13:44 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheVoid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
