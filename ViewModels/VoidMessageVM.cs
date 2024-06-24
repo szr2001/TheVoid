bfedc29833cb38cdf37f4b5f64d0bc8eeaddfe28
Szabo Robert <szr2001@yahoo.com>
2024-06-24 23:07:53 +0300

    Added Animations

--- ViewModels/VoidMessageVM.cs
+++ ViewModels/VoidMessageVM.cs
@@ -5,7 +5,7 @@ namespace TheVoid.ViewModels
 {
     public class VoidMessageVM
     {
-        [MaxLength(2500, ErrorMessage = "Message is too long")]
+        [MaxLength(1500, ErrorMessage = "Message is too long")]
         [MinLength(50, ErrorMessage ="Message is too short")]
         [Required(ErrorMessage = "Message is required")]
         [DisplayName("Void Message")]
